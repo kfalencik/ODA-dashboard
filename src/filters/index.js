037b18b879c4bcef088899dc1902be419fd72625
kfalencik <kfalencik@gmail.com>
2020-05-15 14:27:55 +0100

    Add captialize and date formater filters

---
+++ src/filters/index.js
@@ -0,0 +1,16 @@
+// Vue filters
+import Vue from 'vue';
+import moment from 'moment';
+
+// Capialize first letter
+Vue.filter('capitalize', function (value) {
+  if (!value) return '';
+  value = value.toString();
+  return value.charAt(0).toUpperCase() + value.slice(1);
+})
+
+// Format date
+Vue.filter('dateFromatter', function (value) {
+  if (!value) return '';
+  return moment(value).format('MM/DD/YYYY, H:mm');
+})
