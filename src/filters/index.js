aaa2dde1e745b9bb7eaa9f1e059d46e92a167301
kfalencik <kfalencik@gmail.com>
2020-05-17 17:08:22 +0100

    Use arrow functions in filters

--- src/filters/index.js
+++ src/filters/index.js
@@ -3,14 +3,14 @@ import Vue from 'vue';
 import moment from 'moment';
 
 // Capialize first letter
-Vue.filter('capitalize', function (value) {
+Vue.filter('capitalize',  (value) => {
   if (!value) return '';
   value = value.toString();
   return value.charAt(0).toUpperCase() + value.slice(1);
 })
 
 // Format date
-Vue.filter('dateFromatter', function (value) {
+Vue.filter('dateFromatter', (value) => {
   if (!value) return '';
   return moment(value).format('MM/DD/YYYY, H:mm');
 })
