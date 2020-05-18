de9569c63e7c4cf4709af1063403ba7a68c0447c
kfalencik <kfalencik@gmail.com>
2020-05-18 14:13:47 +0100

    New logo and favicon

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <h1>Welcome to ODA dashboard</h1>
+    <h1>Welcome to Owlbot dashboard</h1>
     <p>An Owlbot Disctionary API app that lets you look up words and their definitions. This app will also display statistics and keep track of your query history.</p>
     <router-link class="button button--primary" to="/queries">Make a query</router-link>
   </div>
